711baf354f597efa0e8a48913f908a137bc544b1
Boris <95616326+zebre13@users.noreply.github.com>
2022-12-27 23:16:46 +0100

    added new task story

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new'
   get 'tasks/:id', to: 'tasks#show', as: :task
+  post 'tasks', to: 'tasks#create'
 end
