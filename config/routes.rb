e0169480dcc7017470ab8ea890e6b464287a3d20
Boris <95616326+zebre13@users.noreply.github.com>
2022-12-28 00:03:31 +0100

    added edit story

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get 'tasks/new', to: 'tasks#new'
   get 'tasks/:id', to: 'tasks#show', as: :task
   post 'tasks', to: 'tasks#create'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
 end
